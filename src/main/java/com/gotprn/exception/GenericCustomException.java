601a730c11fe7df0e4474537d33096500f50a3ea
abhishek <abhishek.uniyal@hotmail.com>
2018-05-16 12:12:09 +0530

    #Creating a Generic Exception to handle Application specific exceptions

---
+++ src/main/java/com/gotprn/exception/GenericCustomException.java
@@ -0,0 +1,13 @@
+package com.gotprn.exception;
+
+/**
+Author Abhishek Uniyal
+Created Date May 16, 2018
+Created Time 12:10:29 PM
+*/
+
+public class GenericCustomException extends Exception{
+	public GenericCustomException(String msg) {
+		super(msg);
+	}
+}
