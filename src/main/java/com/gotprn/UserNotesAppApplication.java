5e47e5429e6ca719a7032bfee2d45eec5af0a254
abhishek <abhishek.uniyal@hotmail.com>
2018-05-16 10:38:32 +0530

    Initial commit

---
+++ src/main/java/com/gotprn/UserNotesAppApplication.java
@@ -0,0 +1,12 @@
+package com.gotprn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserNotesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserNotesAppApplication.class, args);
+	}
+}
